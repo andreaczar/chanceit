4b1aaa68807eb75491632b9a78725b4d0b901fd8
andrea <andrea.czar@gmail.com>
2016-05-30 22:08:50 -0700

    Refactor roll to use proper h file

---
+++ roll.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "roll.h"
+
+int roll(int numDice)
+{
+    srand(time(NULL));
+
+    if(numDice == 2)
+    {
+        return(rand() % (13-2)+2);
+    }
+    if(numDice == 1)
+    {
+        return(rand() % (7-1)+1);
+    }
+}
--- roll.h
+++ roll.h
@@ -1,17 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
-
-
-int roll(int numDice)
-{
-	if(numDice == 2)
-	{
-		return(rand() % (13-2)+2);
-	}
-	if(numDice == 1)
-	{
-		return(rand() % (7-1)+1);
-	}
-}
+// pre and post conditions here
+int roll(int numDice);
