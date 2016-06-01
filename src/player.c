2bf6a3f9070886ac838a2953921c37f43d9d6b35
andrea <andrea.czar@gmail.com>
2016-06-01 16:27:50 -0700

    edited player

--- src/player.c
+++ src/player.c
@@ -15,7 +15,6 @@ Player* initPlayer(char* name){
     player->totalScore = 0;
     player->point = 0;
     player->lastRoll = 0;
-    player->firstRoll = 0;
     player->rollNum = 0;
     player->name = name;
     return player;
