75b9bd8af76e4d2f5f67f60e5149c3cb229bfbb8
Tyler Francis <cst528@deepblue.cs.camosun.bc.ca>
2016-06-10 10:51:29 -0700

    Socket and Protocol Added

---
+++ src/protocol.c
@@ -0,0 +1,23 @@
+//Protocol.c
+//#include stuff here
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+int getOpponent(char *opp){
+	char reply [200];
+	int bytes = readLine(reply);
+	if (bytes > 0){
+		return sscanf(reply, "Opponent: %s\n", opp);
+	}
+	return 0;
+}
+
+int isGameOver (char *response){
+	if (strncmp(response, "You win!", 8) == 0 || strncmp(response, "You Lose", 8) == 0){
+		return 1;
+	} 
+	return 0;
+}
